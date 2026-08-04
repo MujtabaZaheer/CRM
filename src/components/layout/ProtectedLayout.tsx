70bf3b2852e0dd54c883cf7ad2e2ed92ef712737
Mujtaba Zaheer <mujtabazaheer47@gmail.com>
2026-08-05 03:08:44 +0500

    feat: add custom scrollbar styles, fade-in page transitions, and glassmorphism utility classes

--- src/components/layout/ProtectedLayout.tsx
+++ src/components/layout/ProtectedLayout.tsx
@@ -35,7 +35,7 @@ export const ProtectedLayout: React.FC = () => {
       />
       <div className="flex-1 flex flex-col min-w-0">
         <Topbar setMobileOpen={setMobileOpen} />
-        <main className="flex-1 p-4 sm:p-6 lg:p-8 overflow-y-auto">
+        <main className="flex-1 p-4 sm:p-6 lg:p-8 overflow-y-auto animate-fade-in">
           <Outlet />
         </main>
       </div>
